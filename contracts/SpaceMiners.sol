e6e4d560d80ad31d952b1138ea0f40ddf05c5b0a
caleb-berry <calebster.j@gmail.com>
2021-11-24 09:47:12 +0000

    added chainlink vrf contract, warp(), and payout() to SpaceMiners.sol

--- contracts/SpaceMiners.sol
+++ contracts/SpaceMiners.sol
@@ -1,6 +1,43 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
+ 
+contract RandomNumberConsumer is VRFConsumerBase {
+    
+    bytes32 internal keyHash;
+    uint256 internal fee;
+    
+    uint256 public randomResult;
+    
+    constructor() 
+        VRFConsumerBase(
+            0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B, // VRF Coordinator
+            0x01BE23585060835E02B77ef475b0Cc51aA1e0709  // LINK Token
+        )
+    {
+        keyHash = 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311;
+        fee = 0.1 * 10 ** 18; // 0.1 LINK (Varies by network)
+    }
+    
+    /** 
+     * Requests randomness 
+     */
+    function getRandomNumber() public returns (bytes32 requestId) {
+        require(LINK.balanceOf(address(this)) >= fee, "Not enough LINK - fill contract with faucet");
+        return requestRandomness(keyHash, fee);
+    }
+
+    /**
+     * Callback function used by VRF Coordinator
+     */
+    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+        randomResult = randomness;
+    }
+
+    // function withdrawLink() external {} - Implement a withdraw function to avoid locking your LINK in the contract
+}
+
 /// @title ERC1155 ownerOf() for SpaceMiners portals contract
 interface Portal {
     function ownerOf(uint256 tokenId) external view returns (address);
@@ -9,18 +46,24 @@ interface Portal {
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
 /// @title SpaceMiners contract for miners
-contract SpaceMiners is ERC1155 {
+contract SpaceMiners is ERC1155, RandomNumberConsumer {
+
+    event Warp(address indexed _owner, uint _minerId, uint _activeMinerCount);
+    event FeePayout(address indexed _minerOwner, address indexed _portalOwner, uint amount);
 
     struct Miner {
         uint bagSize;
         uint warpFee;
         uint returnTime;
-        uint departTime;
         uint fee;
     }
 
+
     Miner[] public miners;
     mapping(address => mapping(uint => uint)) private activeMiners;
+    mapping(address => mapping(uint => uint)) private minersDepartedTime;
+    mapping(address => uint) private departedCount;
+    mapping(address => uint) private payoutCount;
 
     uint256 public constant MINER_1 = 0;
     uint256 public constant MINER_2 = 1;
@@ -28,10 +71,10 @@ contract SpaceMiners is ERC1155 {
     uint256 public constant MINER_4 = 3;
 
     constructor() ERC1155("https://game.example/api/item/") {
-        miners.push(Miner({bagSize: 16, warpFee: 5, returnTime: 48, departTime: 0, fee: 1 ether}));
-        miners.push(Miner({bagSize: 24, warpFee: 4, returnTime: 39, departTime: 0, fee: 1 ether}));
-        miners.push(Miner({bagSize: 32, warpFee: 4, returnTime: 31, departTime: 0, fee: 1 ether}));
-        miners.push(Miner({bagSize: 48, warpFee: 3, returnTime: 24, departTime: 0, fee: 1 ether}));
+        miners.push(Miner({bagSize: 16, warpFee: 5, returnTime: 48, fee: 1 ether}));
+        miners.push(Miner({bagSize: 24, warpFee: 4, returnTime: 39, fee: 1 ether}));
+        miners.push(Miner({bagSize: 32, warpFee: 4, returnTime: 31, fee: 1 ether}));
+        miners.push(Miner({bagSize: 48, warpFee: 3, returnTime: 24, fee: 1 ether}));
     }
 
     /// @notice Mint miner for fee
@@ -47,6 +90,36 @@ contract SpaceMiners is ERC1155 {
     function checkPortalOwner(uint _id, address _contractAddress) internal view returns(address) {
         Portal portalsContract = Portal(_contractAddress);
         return portalsContract.ownerOf(_id);
+    }
+
+    /// @notice Returns amount of active miner(s) for specified id
+    /// @param _minerId id of the miner (0-3)
+    function getActiveMiners(uint _minerId) public returns(uint) {
+        return activeMiners[msg.sender][_minerId];
+    }
+
+    /// @notice Send a miner through a random portal for GEMs
+    /// @param _minerId id of the miner (0-3)
+    function warp(uint _minerId) public {
+        require(balanceOf(msg.sender, _minerId) - activeMiners[msg.sender][_minerId] >= 1, "You don't own any inactive miners");
+        require(activeMiners[msg.sender][_minerId] <= 10, "Maximum of 10 miners warped per id");
+        minersDepartedTime[msg.sender][departedCount[msg.sender]] = block.timestamp;
+        activeMiners[msg.sender][_minerId]++;
+        departedCount[msg.sender]++;
+        //getRandomNumber(); // Disabled unless running on Rinkeby and can load RandomNumberConsumer with LINK
+    }
+
+    /// @notice Payout to miner owner and portal owner if miner trip time has passed
+    /// @param _minerId id of the miner (0-3)
+    function payout(uint _minerId) public {
+        uint counter;
+        for(uint i=0; i < activeMiners[msg.sender][_minerId]; i++) {
+            if(minersDepartedTime[msg.sender][payoutCount[msg.sender]] - block.timestamp >= miners[_minerId].returnTime * 60) {
+                payoutCount[msg.sender]++;
+                counter++;
+            }
+        }
+        activeMiners[msg.sender][_minerId] -= counter;
     }
 
     function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) internal virtual override {
--- test/spaceminers.js
+++ test/spaceminers.js
@@ -2,7 +2,7 @@ const SpaceMiners = artifacts.require("SpaceMiners");
 const Portals = artifacts.require("Portals");
 
 contract("SpaceMiners", async accounts => {
-  it("should mint 1 miner when payment is >= ETH fee", async () => {
+  it("should mint 1 miner when payment = ETH fee", async () => {
     const instance = await SpaceMiners.deployed();
     const mintMiner = await instance.mintMiner(1, {value: web3.utils.toWei('1', 'ether')});
     const balanceOf = await instance.balanceOf(accounts[0], 1);
@@ -11,4 +11,13 @@ contract("SpaceMiners", async accounts => {
     assert.equal(balanceOf, 1, "miner balance still 0");
     assert.equal(tx.value, web3.utils.toWei('1', 'ether'), "check transaction value");
   });
+  //loop through all miners
+  // it("should warp miner through portal, and prevent warping if: inactive miners > active miners", async () => {
+  //   const instance = await SpaceMiners.deployed();
+  //   const mintMiner = await instance.mintMiner(0, {value: web3.utils.toWei('1', 'ether')});
+  //   const warp = await instance.warp(0);
+  //   const activeMiners = await instance.getActiveMiners(0);
+    
+  //   assert.equal(activeMiners.valueOf(), 1, "activeMiners count still 0");
+  // });
 });
